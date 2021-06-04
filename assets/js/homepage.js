82de2c6334c001b9ce098f5efc2945d81d7b70a4
Thomas Begush <tbegush@gmail.com>
2021-06-03 19:42:26 -0700

    error correction

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,11 +9,20 @@ var getUserRepos = function (user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
   // make a get request to url
-  fetch(apiUrl).then(function (response) {
-    console.log(response);
-    response.json().then(function (data) {
-      displayRepos(data, user);
-    });
+  fetch(apiUrl)
+  .then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data, user);
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  })
+  .catch(function(error) {
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    alert("Unable to connect to GitHub");
   });
 };
 
@@ -36,6 +45,11 @@ var displayRepos = function (repos, searchTerm) {
 };
 
 var displayRepos = function (repos, searchTerm) {
+  // check if api returned any repos
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
   // clear old content
   repoContainerEl.textContent = "";
   repoSearchTerm.textContent = searchTerm;
