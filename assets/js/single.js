ee9d93aff0e4afc7028be8dcaae95c5070833f89
Thomas Begush <tbegush@gmail.com>
2021-06-04 19:38:41 -0700

    added inter page functionality

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,14 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function (){
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  getRepoIssues(repoName);
+  repoNameEl.textContent = repoName;
+};
 
 var getRepoIssues = function(repo) {
   // format the github api url
@@ -81,4 +89,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName()
